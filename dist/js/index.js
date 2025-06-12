660b0cc61efb4fcd3a4cbe3dd81817eef694587b
Luís Martins <137302218+luismrtns@users.noreply.github.com>
2025-06-12 15:48:44 -0300

    adicionei os arquivos

---
+++ dist/js/index.js
@@ -0,0 +1,29 @@
+"use strict";
+let love = document.querySelector(".love");
+let cacto = document.querySelector(".cacto");
+let cacto2 = document.querySelector(".cacto2");
+let jump = () => {
+    love?.classList.add("jump");
+    setTimeout(() => {
+        love?.classList.remove("jump");
+    }, 500);
+};
+let loop = setInterval(() => {
+    let cactoPosition = cacto?.offsetLeft;
+    let cacto2Position = cacto2?.offsetLeft;
+    let lovePosition = Number(window.getComputedStyle(love).bottom.replace("px", ""));
+    if (cactoPosition <= 80 && cactoPosition > 0 && lovePosition < 100) {
+        cacto.style.animation = "none";
+        cacto.style.left = `${cactoPosition}px`;
+    }
+    if (cacto2Position <= 50 && cacto2Position > 0 && lovePosition < 105) {
+        cacto2.style.animation = "none";
+        cacto2.style.left = `${cacto2Position}px`;
+        love.style.animation = "none";
+        love.style.bottom = `${lovePosition}px`;
+        love.src = "img/girl_pixel_art_death.gif";
+        love.style.width = "160px";
+        clearInterval(loop);
+    }
+});
+document.addEventListener("keydown", jump);
---
+++ src/index.ts
@@ -0,0 +1,38 @@
+let love = document.querySelector(".love") as HTMLImageElement;
+let cacto = document.querySelector(".cacto") as HTMLElement;
+let cacto2 = document.querySelector(".cacto2") as HTMLElement;
+
+let jump = () => {
+  love?.classList.add("jump");
+  setTimeout(() => {
+    love?.classList.remove("jump");
+  }, 500);
+};
+
+let loop = setInterval(() => {
+  let cactoPosition = cacto?.offsetLeft;
+  let cacto2Position = cacto2?.offsetLeft;
+  let lovePosition = Number(
+    window.getComputedStyle(love).bottom.replace("px", "")
+  );
+
+  if (cactoPosition <= 80 && cactoPosition > 0 && lovePosition < 100) {
+    cacto.style.animation = "none";
+    cacto.style.left = `${cactoPosition}px`;
+  }
+
+  if (cacto2Position <= 50 && cacto2Position > 0 && lovePosition < 105) {
+    cacto2.style.animation = "none";
+    cacto2.style.left = `${cacto2Position}px`;
+
+    love.style.animation = "none";
+    love.style.bottom = `${lovePosition}px`;
+
+    love.src = "img/girl_pixel_art_death.gif";
+    love.style.width = "160px";
+
+    clearInterval(loop);
+  }
+});
+
+document.addEventListener("keydown", jump);
